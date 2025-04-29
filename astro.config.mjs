805604cbbb7eeaef9f3161ca1e613afa45dd5195
Shane Duff <shaned@hey.com>
2025-04-29 11:51:01 -0700

    Add prism code highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    markdown: {
+        syntaxHighlight: 'prism'
+    },
     site: "https://shaneis.me"
 });
