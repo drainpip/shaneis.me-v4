469b064d9284be7c6994fcd02a1640bd241213ec
Shane Duff <shaned@hey.com>
2025-04-29 10:26:39 -0700

    Set up collection

---
+++ src/content.config.ts
@@ -0,0 +1,16 @@
+// Import the glob loader
+import { glob } from "astro/loaders";
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// Define a `loader` and `schema` for each collection
+const blog = defineCollection({
+    loader: glob({ pattern: '**/[^_]*.md', base: "./src/pages" }),
+    schema: z.object({
+      title: z.string(),
+      date: z.string(),
+      description: z.string(),
+      tags: z.array(z.string())
+    })
+});
+// Export a single `collections` object to register your collection(s)
+export const collections = { blog };
