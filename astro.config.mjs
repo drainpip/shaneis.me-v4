c49256667a664537fd001000089af6892a68a1c4
Shane Duff <shaned@hey.com>
2025-05-19 15:29:53 -0700

    Updated robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import aiRobotsTxt from 'astro-ai-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://shaneis.me",
-    markdown: {
-        shikiConfig: {
-          themes: {
-            light: 'github-light',
-            dark: 'github-dark',
-          },
-          wrap: true,
+  site: "https://shaneis.me",
+  markdown: {
+      shikiConfig: {
+        themes: {
+          light: 'github-light',
+          dark: 'github-dark',
         },
+        wrap: true,
       },
+    },
+  integrations: [aiRobotsTxt()],
 });
